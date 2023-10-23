894f2bbedfcd366fcf7b10296ddc39ab535d4deb
Manel Sole <msoler92@gmail.com>
2023-10-23 09:25:01 +0200

    N1.6 i N1.7 modificats per adequar-se millor a l'enunciat

--- main/java/n1exercici6/Main.java
+++ main/java/n1exercici6/Main.java
@@ -2,11 +2,12 @@ package n1exercici6;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main (String[] args) {
-        List<String> stringList = new ArrayList<>(Arrays.asList("ana", "87989", "pere", "jordi", "897870700", "joan", "sira", "noemi", "marco", "ona", "121", "173626", "maria", "dolors", "josep"));
-        stringList.stream().sorted((s1, s2) -> s1.length() - s2.length()).forEach(System.out::println);
+        List<Object> stringList = new ArrayList<>(Arrays.asList("ana", 87989, "pere", "jordi", 897870700, "joan", "sira", "noemi", "marco", "ona", 121, 173626, "maria", "dolors", "josep"));
+        stringList.stream().filter(s -> s instanceof String).sorted(Comparator.comparingInt(s -> ((String) s).length())).forEach(System.out::println);
     }
 }
--- main/java/n1exercici7/Main.java
+++ main/java/n1exercici7/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Main {
     public static void main (String[] args) {
-        List<String> stringList = new ArrayList<>(Arrays.asList("ana", "87989", "pere", "jordi", "897870700", "joan", "sira", "noemi", "marco", "ona", "121", "173626", "maria", "dolors", "josep"));
-        stringList.stream().sorted((s1, s2) -> s2.length() - s1.length()).forEach(System.out::println);
+        List<Object> stringList = new ArrayList<>(Arrays.asList("ana", 87989, "pere", "jordi", 897870700, "joan", "sira", "noemi", "marco", "ona", 121, 173626, "maria", "dolors", "josep"));
+        stringList.stream().filter(s -> s instanceof String).sorted((s1, s2) -> ((String) s2).length() - ((String)s1).length()).forEach(System.out::println);
     }
 }
